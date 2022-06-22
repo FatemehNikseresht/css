dd8f11809ee8bc4202ef92bbfe6ef5202f71e9a5
fatemeh <fnikseresht71@gmail.com>
2022-06-22 20:41:22 +0430

    Changes to be committed:

---
+++ src/Name.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Name.css';
+
+class Name extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>Fatemeh Nikseresht</h1>
+            </div>
+        )
+    }
+}
+export default Name;
